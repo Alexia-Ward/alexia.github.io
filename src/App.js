7d42440620bc4619a5d2697e4bc4b48d8f020ed7
Alexia Ward <ward.alexia1@gmail.com>
2023-12-05 21:11:09 -0500

    Testing 2

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My Simple React App</h1>
+      <p>This is a simple React app created for Problem 3.</p>
+      <br></br>
+      <h1>Install Process</h1>
+      <p> I installed react by getting nodejs and running commands in the terminal </p>
+      <p> It was actually a lot easier than I thought it would be!</p>
+      <p> I built the page through VSCODE and changed the given files to suit my needs.</p>
+      <br></br>
+      <h1>Difficulties encountered</h1>
+      <p>The first difficulty was understanding what I was looking at after I first installed it.</p>
+      <p>I didn't understand why it kept opening to that weird site in the terminal until I did some research and realized it doesn't give you a blank site when you install</p>
+      <p>I also had to get used to the syntax, its not very different but seeing function App and the other given files made it very confusing</p>
+      <p>Lastly, I have never hosted through github, I've only used it once in the past for a group assignment, so I had to learn how to that as well.</p>
+      <p> It didn't work at first which was very frustrating.</p>
+
+    <img
+    src="https://i.imgur.com/xZXKzamg.jpg"
+    alt="Spinning Image"
+    className="spinning-image App-logo"
+    />
+    </div>
+  );
+}
+
+export default App;
